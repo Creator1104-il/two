9ada99fe3d2b750ec61d4a532318efaec094025b
Creator1104-il <ila04112007@gmail.com>
2025-01-31 09:58:56 +0300

    Add files via upload

---
+++ LR2/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scaner
+        Publick Main{
+    Publick static void Main(string[] agrs)
+        {
+            Scaner in = new Scaner(System.in);
+            System.out.print("Введите x");
+            int x = in.nextInt();
+            System.out.print("Введите n")
+
+        }
+                }
