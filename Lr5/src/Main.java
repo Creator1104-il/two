8ba8a507f2f433714699130b77869454780f15d1
Creator1104-il <ila04112007@gmail.com>
2025-02-27 19:28:12 +0300

    Add files via upload

---
+++ Lr5/src/Main.java
@@ -0,0 +1,62 @@
+class Reader {
+    private String Polnoe_name;
+    private String nomer_bileta;
+    private String facultet;
+    private String den_roschedia;
+    private String nomer_telefona;
+    public Reader(String fullName, String ticketNumber, String faculty, String birthDate, String phone) {
+        this.Polnoe_name = fullName;
+        this.nomer_bileta = ticketNumber;
+        this.facultet = faculty;
+        this.den_roschedia = birthDate;
+        this.nomer_telefona = phone;
+    }
+    public void takeBook(int numberOfBooks) {
+        System.out.println(Polnoe_name + " взял " + numberOfBooks + " книги.");
+    }
+
+    public void takeBook(String[] bookNames) {
+        System.out.print(Polnoe_name + " взял книги: ");
+        for (int i = 0; i < bookNames.length; i++) {
+            System.out.print(bookNames[i]);
+            if (i < bookNames.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(".");
+    }
+
+    public void returnBook(int numberOfBooks) {
+        System.out.println(Polnoe_name + " вернул " + numberOfBooks + " книги.");
+    }
+
+    public void returnBook(String[] bookNames) {
+        System.out.print(Polnoe_name + " вернул книги: ");
+        for (int i = 0; i < bookNames.length; i++) {
+            System.out.print(bookNames[i]);
+            if (i < bookNames.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(".");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Reader[] readers = new Reader[5];
+        readers[0] = new Reader("Петров В. В.", "0001", "Физика", "01.01.2000", "1234567890");
+        readers[1] = new Reader("Иванов И. И.", "0002", "Математика", "02.02.2001", "0987654321");
+        readers[2] = new Reader("Сидоров С. С.", "0003", "Химия", "03.03.2002", "1122334455");
+        readers[3] = new Reader("Кузнецова А. А.", "0004", "Биология", "04.04.2003", "2233445566");
+        readers[4] = new Reader("Смирнов П. П.", "0005", "История", "05.05.2004", "3344556677");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(readers[i]);
+        }
+
+        readers[0].takeBook(3);
+        readers[0].takeBook(new String[]{"Приключения", "Словарь", "Энциклопедия"});
+        readers[0].returnBook(2);
+        readers[0].returnBook(new String[]{"Приключения",  "Энциклопедия"});
+    }
+}
